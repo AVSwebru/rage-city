de08885038ab2e355f4d5bd8cae90b01896c503e
remzov <remzovnik@gmail.com>
2019-07-03 16:31:13 +0300

    header adaptation

--- src/js/adaptation.js
+++ src/js/adaptation.js
@@ -4,9 +4,20 @@
   function headerAdapt() {
     let headerControls = document.querySelector('.js-header-controls');
     let headerLeftside = document.querySelector('.js-header-leftside');
-
-    if (window.matchMedia('(max-width: 640px)').matches) {
-      headerLeftside.appendChild(headerControls);
-    }
+    let headerRightside = document.querySelector('.js-header-rightside');
+    window.addEventListener('DOMContentLoaded', () => {
+      if (window.matchMedia('(max-width: 640px)').matches) {
+        headerLeftside.appendChild(headerControls);
+      } else {
+        headerRightside.appendChild(headerControls);
+      }
+    });
+    window.addEventListener('resize', () => {
+      if (window.matchMedia('(max-width: 640px)').matches) {
+        headerLeftside.appendChild(headerControls);
+      } else {
+        headerRightside.appendChild(headerControls);
+      }
+    });
   }
 })();
